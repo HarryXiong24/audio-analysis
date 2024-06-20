58f85e50037d9981cba6e3daa23cc850e9ae705d
Harry Xiong <harryxiong24@gmail.com>
2024-06-19 20:46:12 -0700

    fix: type

--- src/lib/format-time.ts
+++ src/lib/format-time.ts
@@ -1,4 +1,4 @@
-export const formatTime = (seconds) =>
+export const formatTime = (seconds: number) =>
   [seconds / 60, seconds % 60]
     .map((v) => `0${Math.floor(v)}`.slice(-2))
     .join(":");
